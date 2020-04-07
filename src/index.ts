b827e25ad908870d37880d904df5a054ac73ebef
Matt Isenhower <matt@isenhower.com>
2020-04-07 13:50:47 -0700

    Fix missing exports

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,6 @@
+export { Color } from './Color';
+export { ColorPalette } from './ColorPalette';
 export { Design } from './Design';
 export { DesignType } from './DesignType';
+export { DesignUsage } from './DesignUsage';
 export { QRData } from './QRData';
